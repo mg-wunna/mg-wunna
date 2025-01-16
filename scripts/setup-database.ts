ad3f1524a7e2a495ce0b17515c383413a8c8ba4d
Wunna <mgwunna.mw@icloud.com>
2025-01-16 17:20:35 +0630

    feat: add database connection in setup database script

--- scripts/setup-database.ts
+++ scripts/setup-database.ts
@@ -1,10 +1,59 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 console.log(
   '\x1b[36m%s\x1b[0m',
   `
-╔═══════════════════════════════════════╗
-║                                       ║
-║          Setting Up Database          ║
-║                                       ║
-╚═══════════════════════════════════════╝
-`
+══════════════════════════════════════
+                                     
+          Setting Up Database          
+                                      
+══════════════════════════════════════`
 );
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function setupBlogData() {
+  console.log(
+    '\x1b[36m%s\x1b[0m',
+    `
+  ╔════════════════════════════════╗
+  ║      Setting Up Blog Data      ║
+  ╚════════════════════════════════╝`
+  );
+
+  /* === DO SOMETHING === */
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+async function setupProjectData() {
+  /* === DO SOMETHING === */
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+async function setupContactData() {
+  /* === DO SOMETHING === */
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(async () => {
+    await setupBlogData();
+    await setupProjectData();
+    await setupContactData();
+
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      `
+══════════════════════════════════════
+                                      
+  Setting Up Database Successfully
+                                      
+══════════════════════════════════════`
+    );
+  })
+  .finally(() => {
+    mongoose.disconnect();
+    process.exit(0);
+  });
