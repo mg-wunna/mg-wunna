ae4f013131c4e96aa41ec0d0a0832870df87df94
Wunna <mgwunna.mw@icloud.com>
2025-02-06 13:17:18 +0630

    feat: add home page navigation link

--- src/components/header.component.tsx
+++ src/components/header.component.tsx
@@ -121,6 +121,7 @@ function MobileNavigation(
         </div>
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+            <MobileNavItem href="/">Home</MobileNavItem>
             <MobileNavItem href="/about">About</MobileNavItem>
             <MobileNavItem href="/articles">Articles</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
@@ -133,12 +134,15 @@ function MobileNavigation(
 
 function NavItem({
   href,
+  include,
   children,
 }: {
   href: string
+  include?: boolean
   children: React.ReactNode
 }) {
-  let isActive = usePathname() === href
+  const pathname = usePathname()
+  const isActive = include ? pathname.includes(href) : pathname === href
 
   return (
     <li>
@@ -164,8 +168,11 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        <NavItem href="/">Home</NavItem>
         <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
+        <NavItem href="/articles" include>
+          Articles
+        </NavItem>
         <NavItem href="/projects">Projects</NavItem>
       </ul>
     </nav>
